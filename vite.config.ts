e48c1ef7443388a36aff1bc4c7a34b8839261821
ckwlghks123 <ckwlghks12+github2@gmail.com>
2023-08-14 19:14:38 +0900

    feat: axios 클라이언트 생성 및 proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // 프록시 설정 입력
+      "/api": {
+        target: "서버 주소",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});

16afa42c9fcf02e6deaa5c454f962a87b07e5505
ckwlghks123 <ckwlghks12+github2@gmail.com>
2023-08-14 19:18:33 +0900

    fix: axios 타입 추가 및 사용 설명 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 const BASE_URL = "/api";
 const token = "쿠키 던지 로컬스토리지던지 토큰을 가져와서 넣기";
 
-const axiosClient = axios.create({
+const axiosClient: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
@@ -22,5 +22,17 @@ axiosClient.interceptors.request.use((config) => {
 
 // 사용할 컴포넌트에서 import axiosClient from './axiosClient' 해서
 // axiosClient.메소드('경로') 해서 사용 baseURL 은 이미 설정되어 있으므로 URI 만 붙여서 사용해야함
+
+// interface Data {
+//   id: number;
+//   name: string;
+//   // 기타 필요한 필드 추가
+// }
+
+// const [data, setData] = useState<Data | null>(null);
+
+// const response = await axiosClient.get<Data>('/data');
+
+// 이런식으로 사용 !!@!@!@!@!
 
 export default axiosClient;

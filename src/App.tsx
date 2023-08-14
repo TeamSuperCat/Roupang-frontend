7836301c49cf60d68ab5fb79a5c119da26374dcd
ckwlghks123 <ckwlghks12+github2@gmail.com>
2023-08-14 18:48:55 +0900

    fix: 패키치 추가 설치 및 App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import { RouterProvider } from "react-router";
 import { routers } from "./router";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={routers} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <GlobalStyles />
+        <RouterProvider router={routers} />
+        <ReactQueryDevtools />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 

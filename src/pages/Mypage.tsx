855065a8732c6f440e05c03d39a596215a4d7c26
seod4423 <seod4423@gmail.com>
2023-08-17 11:14:11 +0900

    feat #13 레이아웃 임시 저장

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,17 +1,54 @@
 import React, { useRef, useState } from "react";
 import { styled } from "styled-components";
+import ProfileInput from "../components/mypage/ProfileInput";
+import { Link } from "react-router-dom";
+
+const userProfileInfoProps = [
+  {
+    type: "email",
+    name: "email",
+    text: "이메일",
+  },
+  {
+    type: "text",
+    name: "nickname",
+    text: "닉네임",
+  },
+  {
+    type: "phoneNumber",
+    name: "phoneNumber",
+    text: "전화번호",
+  },
+  {
+    type: "address",
+    name: "address",
+    text: "주소",
+  },
+];
 
 const defaultProfilePath = "default_profile.png";
+
+interface Data {
+  email: string;
+  nickname: string;
+  phoneNumber: string | undefined;
+  address: string;
+  profile: string;
+}
+
 const Mypage = () => {
   const fileRef = useRef<HTMLInputElement>(null);
   const [imgSrc, setImgSrc] = useState<string>(defaultProfilePath);
+  const [data, setData] = useState<Data>({
+    email: "",
+    nickname: "",
+    phoneNumber: "",
+    address: "",
+    profile: "",
+  });
 
   const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-
-    //TODO: upload to storage
-    //      setProfile(url)
-
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
     fileReader.onload = (e) => {
@@ -19,13 +56,28 @@ const Mypage = () => {
       setImgSrc(result);
     };
   };
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+  };
+
   const submitHandler = () => {};
+
   return (
     <>
+      <Heading>This is MYpage.</Heading>
       <Container>
-        <Heading>This is MYpage.</Heading>
         <MypageDiv>
-          <SideMenu>left</SideMenu>
+          <SideMenu>
+            <SideHeading>MENU</SideHeading>
+            <SideMenuList>
+              <Link to='#'>asdf</Link>
+              <Link to='#'>asdf</Link>
+              <Link to='#'>asdf</Link>
+              <Link to='#'>asdf</Link>
+              <Link to='#'>asdf</Link>
+            </SideMenuList>
+          </SideMenu>
           <UserProfile>
             <UserProfileContainer>
               <UserInfoForm onSubmit={submitHandler}>
@@ -44,8 +96,8 @@ const Mypage = () => {
                     </ProfileUpload>
                   </Profile>
                   <InputDiv>
-                    {/* {signupInputProps.map((elem, i) => (
-                      <SignupInput
+                    {userProfileInfoProps.map((elem, i) => (
+                      <ProfileInput
                         key={i}
                         name={elem.name}
                         type={elem.type}
@@ -53,7 +105,7 @@ const Mypage = () => {
                         data={data}
                         onChange={inputChangeHandler}
                       />
-                    ))} */}
+                    ))}
                   </InputDiv>
                 </FormInnerDiv>
                 {/* <SignupButton>회원가입</SignupButton> */}
@@ -69,59 +121,65 @@ const Mypage = () => {
 export default Mypage;
 
 const Container = styled.div`
-  background-color: pink;
-  width: 100%;
-  height: 100%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 const Heading = styled.h1`
+  display: flex;
+  justify-content: center;
   font-size: 2rem;
-  font-weight: 600;
+  margin: 3%;
+  padding-bottom: 2%;
+  border-bottom: 1px solid #605e49;
 `;
 
 const MypageDiv = styled.div`
-  background-color: aliceblue;
-  display: flex;
-  height: 700px;
-`;
-
-const SideMenu = styled.div`
-  background-color: aqua;
-`;
-
-const UserProfile = styled.div`
-  background-color: azure;
-`;
-
-const UserProfileContainer = styled.div`
-  background-color: bisque;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const UserInfoForm = styled.form`
-  background-color: green;
+const SideMenu = styled.div`
+  width: 170px;
+  padding: 30px;
+  margin: 30px;
+  border: 1px solid;
+`;
+
+const SideHeading = styled.h1`
+  font-size: 1.5rem;
+  border-bottom: 1px solid #605e49;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+`;
+
+const SideMenuList = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 5px;
 `;
 
+const UserProfile = styled.div``;
+
+const UserProfileContainer = styled.div``;
+
+const UserInfoForm = styled.form``;
+
 const FormInnerDiv = styled.div`
-  background-color: blue;
+  width: 700px;
+  border: 1px solid;
+
   width: 660px;
   display: grid;
   grid-template-columns: 200px 1fr;
   gap: 40px;
 `;
 
-const Profile = styled.div`
-  background-color: red;
-`;
+const Profile = styled.div``;
 
 const PreviewDiv = styled.div`
   width: 100%;
@@ -131,6 +189,11 @@ const PreviewDiv = styled.div`
   display: flex;
   align-items: center;
 
+  box-sizing: border-box;
+  border-radius: 10px;
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+
   img {
     width: 100%;
     height: auto;
@@ -138,28 +201,6 @@ const PreviewDiv = styled.div`
   }
 `;
 
-const ProfileUpload = styled.div`
-  background-color: #fff;
-  width: 120px;
-  height: 30px;
-  border-radius: 4px;
-  color: #605e49;
-  font-weight: 600;
-  font-size: 0.8rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
+const ProfileUpload = styled.div``;
 
-  box-sizing: border-box;
-  border-radius: 10px;
-  border: none;
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-  cursor: pointer;
-`;
-
-const InputDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
+const InputDiv = styled.div``;

b48c5039d2ff168e0b79b0c88fe2d92896c9316e
seod4423 <seod4423@gmail.com>
2023-08-22 03:17:50 +0900

    임시저장

--- src/pages/Seller.tsx
+++ src/pages/Seller.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
@@ -24,9 +24,7 @@ const Seller = () => {
     stock: 0,
   });
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
+  const handleInputChange = (event: ChangeEvent<HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setProduct((prevProduct) => ({
       ...prevProduct,
@@ -35,22 +33,23 @@ const Seller = () => {
   };
 
   const handleMainImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setProduct((prevProduct) => ({
-        ...prevProduct,
-        mainImage: event.target.files[0],
-      }));
-    }
+    const file = event.target.files?.[0];
+    if (file === undefined) return;
+
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      mainImage: file,
+    }));
   };
 
-  const handleImagesChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setProduct((prevProduct) => ({
-        ...prevProduct,
-        images: Array.from(event.target.files),
-      }));
-    }
-  };
+  // const handleImagesChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   if (event.target.files) {
+  //     setProduct((prevProduct) => ({
+  //       ...prevProduct,
+  //       images: Array.from(event.target.files),
+  //     }));
+  //   }
+  // };
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -71,15 +70,11 @@ const Seller = () => {
     }
 
     try {
-      const response = await axios.post(
-        "http://localhost:8080/seller/products/register",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axios.post("http://localhost:8080/seller/products/register", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
       console.log("Response:", response.data);
       // Handle success or show a success message to the user.
@@ -94,85 +89,53 @@ const Seller = () => {
       <form onSubmit={handleSubmit}>
         <h3>상품등록</h3>
         <FormItem>
-          <label htmlFor="category">상품분류</label>
-          <select
-            name="category"
-            id="category"
-            value={product.category}
-            onChange={handleInputChange}
-          >
-            <option value="간식">간식</option>
-            <option value="사료">사료</option>
-            <option value="미용용품">미용용품</option>
-            <option value="패션용품">패션용품</option>
-            <option value="위생용품">위생용품</option>
-            <option value="식기/급수기">식기/급수기</option>
-            <option value="외출용품">외출용품</option>
-            <option value="장난감/훈련용품">장난감/훈련용품</option>
-            <option value="하우스/안전용품">하우스/안전용품</option>
+          <label htmlFor='category'>상품분류</label>
+          <select name='category' id='category' value={product.category} onChange={handleInputChange}>
+            <option value='간식'>간식</option>
+            <option value='사료'>사료</option>
+            <option value='미용용품'>미용용품</option>
+            <option value='패션용품'>패션용품</option>
+            <option value='위생용품'>위생용품</option>
+            <option value='식기/급수기'>식기/급수기</option>
+            <option value='외출용품'>외출용품</option>
+            <option value='장난감/훈련용품'>장난감/훈련용품</option>
+            <option value='하우스/안전용품'>하우스/안전용품</option>
           </select>
         </FormItem>
-        <FormItem className="width_full">
-          <label htmlFor="name">상품명</label>
+        <FormItem className='width_full'>
+          <label htmlFor='name'>상품명</label>
           <input
-            type="text"
-            id="name"
-            name="name"
+            type='text'
+            id='name'
+            name='name'
             value={product.name}
             onChange={handleInputChange}
             //   onBlur={nameInputBlurHandler}
           />
         </FormItem>
-        <FormItem className="width_full">
-          <label htmlFor="description">상세설명</label>
+        <FormItem className='width_full'>
+          <label htmlFor='description'>상세설명</label>
           {/* <input type="text" id="description" /> */}
-          <textarea
-            name="description"
-            value={product.description}
-            onChange={handleInputChange}
-          />
+          <textarea name='description' value={product.description} onChange={handleInputChange} />
         </FormItem>
         <FormItem>
-          <label htmlFor="image">대표이미지</label>
-          <input
-            type="file"
-            id="image"
-            accept="image/*"
-            onChange={handleMainImageChange}
-          />
+          <label htmlFor='image'>대표이미지</label>
+          <input type='file' id='image' accept='image/*' onChange={handleMainImageChange} />
         </FormItem>
         <FormItem>
-          <label htmlFor="image">상세이미지</label>
-          <input
-            type="file"
-            id="image"
-            accept="image/*"
-            multiple
-            onChange={handleMainImageChange}
-          />
+          <label htmlFor='image'>상세이미지</label>
+          <input type='file' id='image' accept='image/*' multiple onChange={handleMainImageChange} />
         </FormItem>
         <FormItem>
-          <label htmlFor="price">판매가(원)</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            value={product.price}
-            onChange={handleInputChange}
-          />
+          <label htmlFor='price'>판매가(원)</label>
+          <input type='number' id='price' name='price' value={product.price} onChange={handleInputChange} />
         </FormItem>
         <FormItem>
-          <label htmlFor="stock">재고수량</label>
-          <input
-            type="number"
-            id="stock"
-            name="stock"
-            value={product.stock}
-            onChange={handleInputChange}
-          />
+          <label htmlFor='stock'>재고수량</label>
+          <input type='number' id='stock' name='stock' value={product.stock} onChange={handleInputChange} />
         </FormItem>
         <ButtonEditWarp>
-          <button type="submit">등록완료</button>
+          <button type='submit'>등록완료</button>
         </ButtonEditWarp>
       </form>
       <ProductsLists>
@@ -194,9 +157,9 @@ const Seller = () => {
               <td>1</td>
               <td>P000000R</td>
               <td>
-                <div className="product_name">
-                  <div className="product_name_img_wrap">
-                    <img src="/img/thumbnail.jpg" alt="상품썸네일" />
+                <div className='product_name'>
+                  <div className='product_name_img_wrap'>
+                    <img src='/img/thumbnail.jpg' alt='상품썸네일' />
                   </div>
                   <span>공간활용 고양이 윈도우 해먹 창틀해먹 고양이선반</span>
                 </div>
@@ -205,7 +168,7 @@ const Seller = () => {
               <td>8000</td>
               <td>
                 <button>
-                  <Link to="/selleredit">수정</Link>
+                  <Link to='/selleredit'>수정</Link>
                 </button>
               </td>
             </tr>
@@ -255,8 +218,7 @@ const FormItem = styled.div`
     resize: none;
     box-sizing: border-box;
     padding: 5px 10px;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px,
-      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
     border-radius: 5px;
   }
   label {
@@ -269,8 +231,7 @@ const FormItem = styled.div`
     justify-content: flex-end;
     border: none;
     padding: 0 5px;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px,
-      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
     border-radius: 5px;
     option {
       background-color: #fff;
@@ -284,8 +245,7 @@ const FormItem = styled.div`
     box-sizing: border-box;
     border: none;
     padding: 0 10px;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
     border-radius: 5px;
   }
   input[type="file"] {
@@ -300,8 +260,7 @@ const FormItem = styled.div`
     padding: 5px 10px;
     font-weight: 600;
     border: none;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
     transition: all ease-in-out 0.2s;
     &:hover {
       color: #fff;
@@ -337,8 +296,7 @@ const ProductsLists = styled.section`
       font-weight: 600;
       background-color: #fff;
       border: none;
-      box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-        rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+      box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
       border-radius: 5px;
       color: #757575;
       transition: all ease-in-out 0.2s;
@@ -380,8 +338,7 @@ const ButtonEditWarp = styled.div`
     font-size: 16px;
     font-weight: 600;
     border: none;
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px,
-      rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 8px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
     height: 50px;
     width: 100px;
     border-radius: 10px;

610e6b9f2e812c54cfd453264408b9538ea17e08
laurenCho9 <lauren.choux@gmail.com>
2023-08-25 05:34:00 +0900

    style #3 물품리스트 반응형 레이아웃 완료(더보기 페이지)
    메인화면에서 더보기 클릭시 보여지는 화면의 물품리스트 정렬, 반응형 완료

--- src/components/MainList/stMainList.ts
+++ src/components/MainList/stMainList.ts
@@ -6,8 +6,15 @@ export const ListWrapper = styled.div`
   grid-template-columns: repeat(4, 1fr);
   place-items: center;
   grid-row-gap: 30px;
-  grid-column-gap: 5px;
+  grid-column-gap: 20px;
   margin-top: 10px;
+  @media (max-width: 840px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 14px;
+  }
   .observer {
     width: 100px;
     height: 100px;

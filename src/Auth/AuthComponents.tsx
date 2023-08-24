9c9d2bce764543f2e848d9c9e1aac712df8795db
swp91 <seongwoo4477@gmail.com>
2023-08-25 01:02:04 +0900

    .

--- src/Auth/AuthComponents.tsx
+++ src/Auth/AuthComponents.tsx
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "../hooks/useRouter";
-import { AdminRole, User } from "../types/user";
+// import { User } from "../types/user";
+import Loading from "../components/Loading/Loading";
 
 interface GeneralLayoutProps {
   children: React.ReactNode;
   isAdminPage?: boolean;
 }
 
-const AuthComponents: React.FC<GeneralLayoutProps> = ({
-  children,
-  isAdminPage,
-}) => {
-  const [userProfile, setUserProfile] = useState<User | null>(null);
+const AuthComponents: React.FC<GeneralLayoutProps> = ({ children }) => {
+  const [userProfile, setUserProfile] = useState<boolean | null>(null);
   const { routeTo } = useRouter();
 
   const fetchUserProfile = () => {
-    const 로그인여부 = "쿠키 존재 여부나 백엔드에 로그인 확인 요청";
+    const 로그인여부 = localStorage.getItem("accessToken");
 
-    if (로그인여부 === null) {
+    if (!로그인여부) {
       // 쿠키가 없거나 로그인 확인 요청 했을때 아니면
       routeTo("/login");
       return;
     }
 
-    setUserProfile(null); // 로그인해서 올바른 토큰을 받아 왔거나 올바른 유저 정보를 서버에서 가져왔으면 상태 갱신 나중에 전역상태로 바꿔줘야함
+    setUserProfile(true); // 로그인해서 올바른 토큰을 받아 왔거나 올바른 유저 정보를 서버에서 가져왔으면 상태 갱신 나중에 전역상태로 바꿔줘야함
   };
 
   useEffect(() => {
@@ -31,17 +29,18 @@ const AuthComponents: React.FC<GeneralLayoutProps> = ({
   }, [children]);
 
   //응답으로 받은 user의 userInfo.roles가 비어있다면 아무 권한이 없는 user이므로 로그인 페이지로 이동
-  if (userProfile?.userInfo.roles.length === 0) {
-    routeTo("/login");
-    return <></>;
-  }
-  //Admin 전용 페이지 접근 시도시 userProfile.userInfo.roles에 admin이 없는 경우에는 다른 페이지로 이동
-  if (isAdminPage && !userProfile?.userInfo.roles.includes(AdminRole)) {
-    routeTo("/home");
-    return <></>;
-  }
+  // if (userProfile?.userInfo.roles.length === 0) {
+  //   routeTo("/login");
+  //   return <></>;
+  // }
 
-  if (userProfile === null) return <div>loading...</div>;
+  //Admin 전용 페이지 접근 시도시 userProfile.userInfo.roles에 admin이 없는 경우에는 다른 페이지로 이동
+  // if (isAdminPage && !userProfile?.userInfo.roles.includes(AdminRole)) {
+  //   routeTo("/home");
+  //   return <></>;
+  // }
+
+  if (userProfile === null) return <Loading />;
 
   return (
     <div>

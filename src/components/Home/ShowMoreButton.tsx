4016fab224441cba3e15d225f6f9fcaa5729d516
laurenCho9 <lauren.choux@gmail.com>
2023-08-25 08:29:15 +0900

    style #3 물품리스트 반응형 레이아웃 완료(메인 페이지)
    - 메인페이지 전체 레이아웃 및 ui 작업 완료.
    - 물품리스트 반응형 전체 완료. (더보기페이지, 메인페이지)

--- src/components/Home/ShowMoreButton.tsx
+++ src/components/Home/ShowMoreButton.tsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { SlArrowRight } from "react-icons/sl";
+import { BsArrowRightSquare } from "react-icons/bs";
 
 interface Props {
   onClick: () => void;
@@ -10,7 +10,7 @@ function ShowMore({ onClick }: Props) {
     <ShowMoreBtnWrapper onClick={onClick}>
       <span>더 보기</span>
       <ShowMoreBtn>
-        <SlArrowRight />
+        <BsArrowRightSquare />
       </ShowMoreBtn>
     </ShowMoreBtnWrapper>
   );
@@ -23,23 +23,41 @@ const ShowMoreBtnWrapper = styled.li.attrs({ className: `size` })`
   justify-content: center;
   align-items: center;
   width: 95%;
-  min-height: 350px;
+  /* min-height: 350px; */
   cursor: pointer;
+  position: relative;
   & span {
+    position: absolute;
+    top: 2.4px;
+    left: 0;
     font-size: 12px;
+    font-weight: 600;
+    @media (max-width: 640px) {
+      font-size: 10px;
+      top: 38px;
+      left: 0;
+    }
   }
 `;
 
 const ShowMoreBtn = styled.button`
-  width: 50px;
-  height: 50px;
+  /* width: 50px;
+  height: 50px; */
   border-radius: 50%;
+  position: absolute;
+  top: 0;
+  left: 34px;
   &&&&&&&&&&&&&&& {
     color: black;
     background: none;
     outline: none;
     border: none;
   }
-  font-size: 25px;
+  font-size: 14px;
   cursor: pointer;
+  @media (max-width: 640px) {
+    top: 55px;
+    left: 0;
+    font-size: 18px;
+  }
 `;

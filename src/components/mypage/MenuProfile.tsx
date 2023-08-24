101222527f54313d7d027fd5c8605893a2c660ad
seod4423 <seod4423@gmail.com>
2023-08-25 05:49:45 +0900

    null 처리

--- src/components/mypage/MenuProfile.tsx
+++ src/components/mypage/MenuProfile.tsx
@@ -181,24 +181,12 @@ const ContentsContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  padding: 60px 40px 40px;
+  padding: 60px 40px;
 `;
 
 const UserProfile = styled.div``;
 
-const UserProfileContainer = styled.div`
-  width: 668px;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 10px;
-  gap: 40px;
-  padding: 60px 40px 40px;
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-  border-radius: 24px;
-`;
+const UserProfileContainer = styled.div``;
 
 const UserInfoForm = styled.form`
   display: flex;
@@ -245,7 +233,7 @@ const Profile = styled.div`
 const PreviewDiv = styled.div`
   width: 100%;
   height: 80%;
-  margin: 15% auto 10%;
+  margin: 10% auto 10%;
   overflow: hidden;
   display: flex;
   align-items: center;
